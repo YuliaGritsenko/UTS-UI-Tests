34da7e6afb64f7d8c4f9d3ad0bce8e7953ada15d
YuliaGritsenko <yulia.gritsenko@uts.edu.au>
2025-07-29 11:06:55 +1000

    Update run.js

--- tests/TZLockers_Science_Reports/run.js
+++ tests/TZLockers_Science_Reports/run.js
@@ -1,1 +1,45 @@
+const { By, until } = require("selenium-webdriver");
+const edge = require('selenium-webdriver/edge');
 
+function log(msg) {
+  process.stdout.write(`${msg}\n`);
+}
+
+module.exports = async function(driver, parameters = {}) {
+    // Initialize URLs and credentials
+    const url = "https://cb2lockersci.uts.edu.au";
+    const urlAL = "https://cb2lockersci.uts.edu.au/Report/AvailableLockers";
+
+    const login_lockers = parameters.LOGLOCKERS1 || "";
+    const password_lockers = parameters.PASSLOCKERS1 || "";
+    const username_lockers = parameters.USERLOCKERS1 || "";
+
+    log("🟠 Starting TZ Lockers Add Reservation Test - Science");
+    try {
+        // Launch the Website
+        log(`🌏 Navigating to ${url}`);
+        await driver.get(url);
+        await driver.sleep(2000); // Wait for the page to load
+
+        // Log In as User
+        log("🔐 Logging in as user...");
+        await driver.findElement(By.id("loginUserName")).sendKeys(login_lockers);
+        await driver.findElement(By.id("loginPassword")).sendKeys(password_lockers);
+        await driver.findElement(By.id("loginButton")).click();
+        await driver.sleep(2000); // Wait for login
+
+        // Available Lockers
+        await driver.get(urlAL);
+        await driver.sleep(2000); // Wait for the page to load
+        await driver.findElement(By.id("generateReportBtn")).click();
+
+
+      log("🟢 Locker Reports test steps completed successfully.");
+    } catch (err) {
+        process.stderr.write(`🔥 Fatal test error: ${err && err.message}\n`);
+        throw err;
+    } finally {
+       // Optionally close the browser
+       await driver.quit();
+    }
+};
