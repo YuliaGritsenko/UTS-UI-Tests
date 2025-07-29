8d076983489dd075a4959a5130a35875e7ae9a95
YuliaGritsenko <yulia.gritsenko@uts.edu.au>
2025-07-29 15:19:03 +1000

    Update run.js

--- tests/TZLockers_Science_Reports/run.js
+++ tests/TZLockers_Science_Reports/run.js
@@ -9,7 +9,16 @@ function log(msg) {
 module.exports = async function(driver, parameters = {}) {
     // Initialize URLs and credentials
     const url = "https://cb2lockersci.uts.edu.au";
-    const urlAL = "https://cb2lockersci.uts.edu.au/Report/AvailableLockers";
+
+   // Report Links
+    const urlAL = "https://cb2lockersci.uts.edu.au/Report/AvailableLockers"; // Available Lockers
+    const urlLS = "https://cb2lockersci.uts.edu.au/Report/LeaseSummary"; // Reservations Summary
+    const urlEL = "https://cb2lockersci.uts.edu.au/Report/ExpiredLockers"; // Expired Lockers
+    const urlFL = "https://cb2lockersci.uts.edu.au/Report/FlaggedLockers"; // Flagged Lockers
+    const urlIL = "https://cb2lockersci.uts.edu.au/Report/LockerInactivity; // Inactive Lockers
+    const urlUA = "https://cb2lockersci.uts.edu.au/Report/UserActivities"; // User Activities
+    const urlOA = "https://cb2lockersci.uts.edu.au/Report/OperatorActivities"; // Operator Activities
+    const urlSE = "https://cb2lockersci.uts.edu.au/Report/SystemEvents"; // System Events
 
     const login_lockers = parameters.LOGLOCKERS1 || "";
     const password_lockers = parameters.PASSLOCKERS1 || "";
@@ -30,14 +39,81 @@ module.exports = async function(driver, parameters = {}) {
         await driver.sleep(2000); // Wait for login
 
         // Available Lockers
+        log("📋 Available Lockers Report");
         await driver.get(urlAL);
         await driver.sleep(2000); // Wait for the page to load
         await driver.findElement(By.id("generateReportBtn")).click();
         await driver.sleep(3000); // Wait for the report is generated
           // Take a screenshot
-          let image = await driver.takeScreenshot();
+          // let image = await driver.takeScreenshot();
           // Save the screenshot to a file
-          fs.writeFileSync('screenshot.png', image, 'base64');
+          // fs.writeFileSync('screenshot.png', image, 'base64');
+          log("📋 Screenshot saved (TODO)");
+
+          // Reservations Summary
+          log("📋 Reservations Summary Report");
+          await driver.get(urlLS);
+          await driver.sleep(2000); // Wait for the page to load
+          await driver.findElement(By.id("generateReportBtn")).click();
+          await driver.sleep(3000); // Wait for the report is generated
+          
+          log("📋 Screenshot saved (TODO)");
+
+          // Expired Lockers
+          log("📋 Expired Lockers Report");
+          await driver.get(urlEL);
+          await driver.sleep(2000); // Wait for the page to load
+          await driver.findElement(By.id("generateReportBtn")).click();
+          await driver.sleep(3000); // Wait for the report is generated
+          
+          log("📋 Screenshot saved (TODO)");
+
+          // Flagged Lockers
+          log("📋 Flagged Lockers Report");
+          await driver.get(urlFL);
+          await driver.sleep(2000); // Wait for the page to load
+          await driver.findElement(By.id("generateReportBtn")).click();
+          await driver.sleep(3000); // Wait for the report is generated
+          
+          log("📋 Screenshot saved (TODO)");
+
+          // Inactive Lockers
+          log("📋 Inactive Lockers Report");
+          await driver.get(urlIL);
+          await driver.sleep(2000); // Wait for the page to load
+          await driver.findElement(By.id("generateReportBtn")).click();
+          await driver.sleep(3000); // Wait for the report is generated
+          
+          log("📋 Screenshot saved (TODO)");
+
+          // User Activities
+          log("📋 User Activities Report");
+          await driver.get(urlUA);
+          await driver.sleep(2000); // Wait for the page to load
+          await driver.findElement(By.id("LeaseeSalaryId")).sendKeys(login_lockers);
+          await driver.findElement(By.id("generateReportBtn")).click();
+          await driver.sleep(3000); // Wait for the report is generated
+          
+          log("📋 Screenshot saved (TODO)");
+
+          // Operator Activities
+          log("📋 Operator Activities Report");
+          await driver.get(urlOA);
+          await driver.sleep(2000); // Wait for the page to load     
+          // await driver.findElement(By.id("generateReportBtn")).click();
+          // await driver.sleep(3000); // Wait for the report is generated
+          
+          log("📋 SKIPPED: TODO: select date");
+         
+      
+          // System Events
+          log("📋 System Events Report");
+          await driver.get(urlSE);
+          await driver.sleep(2000); // Wait for the page to load     
+         // await driver.findElement(By.id("generateReportBtn")).click();
+         // await driver.sleep(3000); // Wait for the report is generated
+          
+          log("📋 SKIPPED: TODO: select date");
 
       log("🟢 Locker Reports test steps completed successfully.");
     } catch (err) {
